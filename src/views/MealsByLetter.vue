b83cc07cae9e607949b8d7c0914d1441efb475df
Webdevcitp136 <Webdevcitp136@gmail.com>
2024-04-14 18:36:39 -0500

    search by name is complete

--- src/views/MealsByLetter.vue
+++ src/views/MealsByLetter.vue
@@ -14,7 +14,7 @@ watch(route, () => {
 </script>
 
 <template>
-    <div class="p-8 pb-0 text-orange-500">
+    <div class="p-8 pb-0 text-red-500">
       <h1 class="text-4xl font-bold mb-4"> Search Meals By Letter</h1>
    </div> 
 
